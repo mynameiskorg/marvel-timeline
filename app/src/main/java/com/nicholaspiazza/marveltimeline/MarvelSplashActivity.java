8ffe6215f1d93f9b9e591f91571f753957970829
airpiazza <nick@piazzaonline.com>
2021-11-10 16:46:08 -0800

    splash screen

---
+++ app/src/main/java/com/nicholaspiazza/marveltimeline/MarvelSplashActivity.java
@@ -0,0 +1,25 @@
+package com.nicholaspiazza.marveltimeline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MarvelSplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_marvel_splash);
+        Handler handler = new Handler();
+        Intent intent = new Intent(this, MainActivity.class);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+                finish();
+            }
+        }, 2500);
+    }
+}
