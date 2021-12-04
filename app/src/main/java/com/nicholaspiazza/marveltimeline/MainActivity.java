7dceb09dc5b874c02d073201e3a4fb963c700778
airpiazza <nick@piazzaonline.com>
2021-12-03 19:13:17 -0800

    added action bar

--- app/src/main/java/com/nicholaspiazza/marveltimeline/MainActivity.java
+++ app/src/main/java/com/nicholaspiazza/marveltimeline/MainActivity.java
@@ -1,5 +1,6 @@
 package com.nicholaspiazza.marveltimeline;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -28,6 +29,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.marvel_action_bar);
+
         RecyclerView rvMovies = findViewById(R.id.rvMovies);
 
         movies = new ArrayList<>();
